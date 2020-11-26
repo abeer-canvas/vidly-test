ad56e3f425b52bcf88157a78345ec7693de98f2b
Abeer <abeer.technology@gmail.com>
2020-11-26 13:43:15 +0600

    used winston for logging errors

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,8 @@
+const winston = require('winston');
+
 module.exports = function(err, req, res, next){
+    winston.error(err.message, err);
+
     // 500 = something failed on the server we don't know what.
     res.status(500).send('Something failed.');
 }
