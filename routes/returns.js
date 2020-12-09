b0e07d4bfe084b6576b44e6aafe4170967e75271
Abeer <abeer.technology@gmail.com>
2020-12-09 12:36:57 +0600

    Tested if rental is not found, rental is processed, valid request

--- routes/returns.js
+++ routes/returns.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const auth = require('../middleware/auth');
+const { Rental } = require('../models/rental');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     if(!req.body.customerId) return res.status(400).send('customerId is not provided');
     if(!req.body.movieId) return res.status(400).send('movieId is not provided');
 
-    await res.status(401).send('Unauthorized');
+    const rental = await Rental.findOne({
+        'customer._id': req.body.customerId,
+        'movie._id': req.body.movieId
+    });
+    if(!rental) return res.status(404).send('Rental not found!');
+
+    if(rental.dateReturned) return res.status(400).send('Rental already processed.');
+
+    return res.status(200).send();
 });
 
 module.exports = router;
